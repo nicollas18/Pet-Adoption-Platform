4870c731c3c8179e80203a3e29789b52d4b36403
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2023-12-07 18:25:14 -0300

    Criação da classe animal (gets e sets)

---
+++ Animal.java
@@ -0,0 +1,48 @@
+public class Animal {
+    private String nome;
+    private int idade;
+    private String raca;
+    private String personalidade;
+
+    // Construtor
+    public Animal(String nome, int idade, String raca, String personalidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.raca = raca;
+        this.personalidade = personalidade;
+    }
+
+    // Métodos getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public String getPersonalidade() {
+        return personalidade;
+    }
+
+    // Métodos setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public void setPersonalidade(String personalidade) {
+        this.personalidade = personalidade;
+    }
+}
