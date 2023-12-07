0e3daf77b641f9f9fc0c3b821872b70ba7bae96a
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2023-12-07 18:39:13 -0300

    Criação da classe "Usuário".

---
+++ Usuaio.java
@@ -0,0 +1,38 @@
+public class Usuaio {
+    private String nome;
+    private String email;
+    private String senha;
+
+    // Construtor
+    public Usuaio(String nome, String email, String senha) {
+        this.nome  = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    // Métodos getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    // Métodos setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+        
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
